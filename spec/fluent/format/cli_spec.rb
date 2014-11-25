7f38748a011e032abcd14b21c739427addc502ee
sonots <sonots@gmail.com>
2014-11-26 02:44:43 +0900

    remove rspec deprecated warnings

--- spec/fluent/format/cli_spec.rb
+++ spec/fluent/format/cli_spec.rb
@@ -20,13 +20,13 @@ describe Fluent::Format::CLI do
 
     context "success" do
       let(:opts) { {config: "fluent.conf"} }
-      it { capture_stderr { subject }.should == "" }
+      it { expect(capture_stderr { subject }).to eq "" }
       it { expect { subject }.not_to raise_error }
     end
 
     context "failure" do
       let(:opts) { {config: "unknown.conf"} }
-      it { capture_stderr { subject }.should include "No such file or directory" }
+      it { expect(capture_stderr { subject }).to include "No such file or directory" }
       it { expect { subject }.to raise_error(SystemExit) }
     end
   end
@@ -36,38 +36,38 @@ describe Fluent::Format::CLI do
 
     context "success" do
       let(:opts) { {config: "fluent.conf"} }
-      it { capture_stderr { subject }.should == "" }
+      it { expect(capture_stderr { subject }).to eq "" }
       it { expect { subject }.not_to raise_error }
     end
 
     context "-p option" do
       let(:opts) { {config: "fluent.conf", plugin_dirs: ["example"]} }
-      it { capture_stderr { subject }.should == "" }
+      it { expect(capture_stderr { subject }).to eq "" }
       it { expect { subject }.not_to raise_error }
     end
 
     context "-r option" do
       let(:libs) { [File.expand_path('../../../example/out_example', File.dirname(__FILE__))] }
       let(:opts) { {config: "fluent.conf", libs: libs } }
-      it { capture_stderr { subject }.should == "" }
+      it { expect(capture_stderr { subject }).to eq "" }
       it { expect { subject }.not_to raise_error }
     end
 
     context "syntax error" do
       let(:opts) { {config: "example/syntax_error.conf"} }
-      it { capture_stderr { subject }.should include("parse error") }
+      it { expect(capture_stderr { subject }).to include("parse error") }
       it { expect { subject }.to raise_error(SystemExit) }
     end
 
     context "plugin error" do
       let(:opts) { {config: "example/plugin_error.conf"} }
-      it { capture_stderr { subject }.should include("Unknown input plugin") }
+      it { expect(capture_stderr { subject }).to include("Unknown input plugin") }
       it { expect { subject }.to raise_error(SystemExit) }
     end
 
     context "param error" do
       let(:opts) { {config: "example/param_error.conf", plugin_dirs: ["example"]} }
-      it { capture_stderr { subject }.should include("out_example.rb") }
+      it { expect(capture_stderr { subject }).to include("out_example.rb") }
       it { expect { subject }.to raise_error(SystemExit) }
     end
   end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -5,8 +5,3 @@ require 'fluent-format'
 
 ROOT = File.dirname(__FILE__)
 Dir[File.expand_path("support/**/*.rb", ROOT)].each {|f| require f }
-
-RSpec.configure do |config|
-  config.treat_symbols_as_metadata_keys_with_true_values = true
-  config.run_all_when_everything_filtered = true
-end
